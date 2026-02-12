0a02f0f6e27c50b289f89c76bd4e9776286a2794
goupfollow <goupfollow@gmail.com>
2026-02-12 10:06:34 +0530

    Implement download count feature
    Add download count tracking using localStorage.

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Go Up Follow website loaded");
+let count = localStorage.getItem("apkDownloads") || 0;
+document.getElementById("downloadCount").innerText = count;
+
+function countDownload(){
+  count++;
+  localStorage.setItem("apkDownloads", count);
+  document.getElementById("downloadCount").innerText = count;
+}
